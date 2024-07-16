a5d768c7ab34ecdfb58b2ff63ed2c46b02cebd96
OliRac <olivieracette@gmail.com>
2024-07-16 13:15:03 -0400

    Squashed commit of the following:
    commit a20404ed376cefdba71f4918b3811796c5a45d66
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 13:14:04 2024 -0400
    
        finalizing
    
    commit 4b9cd6f5f2f79e1b6897701c9a569fa61281b22c
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 13:01:57 2024 -0400
    
        add projects
    
    commit 582ec41fffab8c213c6b9664ba9c633ed2044f77
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 12:50:02 2024 -0400
    
        add footer
    
    commit 1a97efec0a7ecf500b4476982e5067952e4e160e
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 12:05:08 2024 -0400
    
        add custom colour, add skills, add experience
    
    commit ab426751b5ebb2b069ad799439632559846b5e7c
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 11:10:18 2024 -0400
    
        more typos...
    
    commit 4095c4f83c168deab3b9c36f73ce08db9cea7ff7
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 11:09:45 2024 -0400
    
        typo in subtitle
    
    commit f3762a6179f3066b8e0e8fdab98d7fe67d591fac
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 11:08:43 2024 -0400
    
        add about
    
    commit c64d9347cb15dd67389515f44968564736d6ca7a
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 10:09:23 2024 -0400
    
        add hero, navbar spacing
    
    commit 5d15c2bbda3d13a3068f1966e9b96e44735b6146
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 09:54:40 2024 -0400
    
        add primary colour
    
    commit 74e5708555e767b07f0afe54ca7c280587b93ef0
    Author: OliRac <olivieracette@gmail.com>
    Date:   Tue Jul 16 09:40:05 2024 -0400
    
        add navbar contents

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Toggle hamburger menu
+(function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
